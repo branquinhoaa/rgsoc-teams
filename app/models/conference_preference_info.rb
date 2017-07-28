9ad1cd98f0ea71a62b62119ca3302ba8eb4b178f
branquinhoaa <amanda.alves15@gmail.com>
2017-07-28 16:58:59 -0300

    conference preference info changes

--- app/models/conference_preference_info.rb
+++ app/models/conference_preference_info.rb
@@ -1,29 +1,24 @@
-# frozen_literal_string: true
-
-# nodoc
 class ConferencePreferenceInfo < ApplicationRecord
-  # validates :conference_id, presence: true
-  # validates :condition_term_ticket presence: true
-  # validates :condition_term_cost presence: true
-  # before_save :accepted_conditions?
-
   belongs_to :team
-  has_many :conference_preferences
+  has_many :conference_preferences, dependent: :destroy
   accepts_nested_attributes_for :conference_preferences,
                                 allow_destroy: true,
                                 reject_if: :without_conferences?
+  validate :accepted_conditions?
 
   def with_preferences_build
-    conference_preferences.build(option: 1) unless conference_preferences.find_by(option: 1)
-    conference_preferences.build(option: 2) unless conference_preferences.find_by(option: 2)
+    team_preferences = []
+    team_preferences << conference_preferences.build(option: 1) unless conference_preferences.find_by(option: 1)
+    team_preferences << conference_preferences.build(option: 2) unless conference_preferences.find_by(option: 2)
+    team_preferences
   end
 
   def without_conferences?(att)
     att[:conference_id].blank?
   end
 
-  # def accepted_conditions?
-  #   return false if condition_term_ticket.blank? && condition_term_cost.blank?
-  #   errors.add(:base, 'Accept terms and conditions is necessary')
-  # end
+  def accepted_conditions?
+    return unless conference_preferences.any?  && (!condition_term_ticket || !condition_term_cost)
+    errors.add(:team, 'you must accept terms and conditions.')
+  end
 end
